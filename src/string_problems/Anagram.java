46daa24411b8e522db9730a68b7340613516e4f2
mehnazbhuiyan <mehnaz68@gmail.com>
2022-10-12 14:25:42 -0400

    Completed Anagram

--- src/string_problems/Anagram.java
+++ src/string_problems/Anagram.java
@@ -1,4 +1,5 @@
 package string_problems;
+import java.util.Arrays;
 
 public class Anagram {
 
@@ -14,4 +15,35 @@ public class Anagram {
      */
 
     // Implement Here
-}
+    public static void main(String[] args) {
+        String str1 = "listen";
+        String str2 = "silent";
+
+        // check if the two strings have equal length
+        if (str1.length() == str2.length()) {
+
+            // if this is true, convert strings to char array
+            char[] charArray1 = str1.toCharArray();
+            char[] charArray2 = str2.toCharArray();
+
+            // sort the char array
+            Arrays.sort(charArray1);
+            Arrays.sort(charArray2);
+
+            // if sorted char arrays are the same,
+            // then the two strings are anagram
+
+            boolean result = Arrays.equals(charArray1, charArray2);
+
+            if (result) {
+                System.out.println(str1 + " and " + str2 + " are anagram");
+            } else {
+                System.out.println(str1 + " and " + str2 + " are not anagram");
+            }
+        }
+            else {
+                System.out.println(str1 + " and " + str2 + " are not anagram ");
+            }
+        }
+    }
+
