9fbac6e880d6c6f7ed827d1f0d6f015b45ae062f
mehnazbhuiyan <mehnaz68@gmail.com>
2022-10-12 14:39:14 -0400

    Completed Patterns

--- src/math_problems/Pattern.java
+++ src/math_problems/Pattern.java
@@ -2,15 +2,33 @@ package math_problems;
 
 public class Pattern {
 
-    /** INSTRUCTIONS
+    /**
+     * INSTRUCTIONS
      * Read the numbers below, identify the pattern, and then implement the logic from this pattern.
      * Once done, you should test to see if you get the same output as below:
-     *
+     * <p>
      * 100,99,98,97,96,95,94,93,92,91,90,88,86,84,82,80,78,76,74,72,70,67,64,61,58,55,52,49,46,43,40,36,32...
-     *
      */
 
     public static void main(String[] args) {
 
+        usePattern();
+    }
+
+    public static void usePattern() {
+        for (int i = 100; i >= 1; i--) {
+
+            if (i < 90) {
+                i--;
+            }
+            if (i < 70) {
+                i--;
+            }
+            if (i < 40) {
+                i--;
+            }
+            System.out.print(i + ", ");
+        }
+        System.out.print("\b" + "\b");
     }
 }
