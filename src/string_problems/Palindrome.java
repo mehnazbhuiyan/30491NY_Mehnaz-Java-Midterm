fbcca2d7a8388ec163c30355683006f4b40077fb
mehnazbhuiyan <mehnaz68@gmail.com>
2022-10-12 17:01:48 -0400

    Completed Palindrome

--- src/string_problems/Palindrome.java
+++ src/string_problems/Palindrome.java
@@ -1,5 +1,5 @@
 package string_problems;
-
+import java.util.Scanner;
 public class Palindrome {
 
     /** INSTRUCTIONS
@@ -11,8 +11,33 @@ public class Palindrome {
      */
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a string to check whether or not it is a palindrome");
+        String input = scanner.nextLine();
+
+        // check if the string is a palindrome
+        if (isPalindrome(input)) {
+            System.out.println(input + " is a palindrome string");
+        } else {
+            System.out.println(input + " is not a palindrome string");
+        }
+    }
+
+    private static boolean isPalindrome(String str) {
+        int left = 0, right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right --;
+        }
+        return true;
 
     }
+
     // Implement here
+
 
 }
